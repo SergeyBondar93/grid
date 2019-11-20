affacba59d92927b054b5a357e4eefe7f54ca6be
Sergey Bondar <sergeyb@xcritical.software>
2019-11-20 13:40:27 +0300

    change generates data

--- src/index.js
+++ src/index.js
@@ -53,18 +53,18 @@ const createDublicateRows = (n) => {
   for (let i = 0; i < n; i++) {
     res.push({
       price: Math.floor(Math.random() * 100000 + 10000),
-      make: generateLorem(Math.floor(Math.random() * 30 + 1)),
+      make: generateLorem(Math.floor(Math.random() * 40 + 1)),
       model: generateLorem(Math.floor(Math.random() * 10 + 1)),
       col1: generateLorem(Math.floor(Math.random() * 20 + 1)),
       col2: generateLorem(Math.floor(Math.random() * 3 + 1)),
       col3: generateLorem(Math.floor(Math.random() * 5 + 1)),
       col4: generateLorem(Math.floor(Math.random() * 15 + 1)),
       col5: generateLorem(Math.floor(Math.random() * 10 + 1)),
-      col6: generateLorem(Math.floor(Math.random() * 20 + 1)),
+      col6: generateLorem(Math.floor(Math.random() * 15 + 1)),
       col7: generateLorem(Math.floor(Math.random() * 2 + 1)),
       col7: generateLorem(Math.floor(Math.random() * 2 + 1)),
       col8: generateLorem(Math.floor(Math.random() * 0 + 1)),
-      col10: generateLorem(Math.floor(Math.random() * 40 + 1)),
+      col10: generateLorem(Math.floor(Math.random() * 10 + 1)),
       row: i
     })
   }
